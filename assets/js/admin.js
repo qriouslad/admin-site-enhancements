24d924b1d1c4fb1ed8c5981ada2a469cb7fc058f
Bowo <wibowosulistio@gmail.com>
2022-10-12 21:12:36 +0700

    Optimize admin menu, page and assets loading and UI

---
+++ assets/js/admin.js
@@ -0,0 +1,16 @@
+(function( $ ) {
+	'use strict';
+
+     $(document).ready( function() {
+
+        var addReview = '<a href="https://wordpress.org/plugins/wp-enhancements/#reviews" target="_blank" class="header-action"><span>&starf;</span> Review</a>';
+        var giveFeedback = '<a href="https://wordpress.org/support/plugin/wp-enhancements/" target="_blank" class="header-action">&#10010; Feedback</a>';
+        var donate = '<a href="https://paypal.me/qriouslad" target="_blank" class="header-action">&#9829; Donate</a>';
+
+        $(addReview).appendTo('.csf-header-left');
+        $(giveFeedback).appendTo('.csf-header-left');
+        $(donate).appendTo('.csf-header-left');
+
+     });
+
+})( jQuery );
