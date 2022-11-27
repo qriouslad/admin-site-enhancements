ded50ee4ac8b84ebc7c32cb8dfd65b19de07b9d8
Bowo <wibowosulistio@gmail.com>
2022-11-27 18:20:11 +0700

    Move code for Disable XML-RPC into Security sections

--- classes/class-security.php
+++ classes/class-security.php
@@ -641,4 +641,16 @@ class Security {
 
 	}
 
+	/**
+	 * Disable the XML-RPC component
+	 *
+	 * @since 2.2.0
+	 */
+	public function maybe_disable_xmlrpc( $data ) {
+
+		http_response_code(403);
+		exit('You don\'t have permission to access this file.');
+
+	}
+
 }
